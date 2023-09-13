e4dd9b9b7c411149b737d38cc6a5a62a448b6477
Aigerim Almazova <71431806+moonbell-e@users.noreply.github.com>
2023-09-13 12:06:05 +0300

    Added percentile calculation in the second version of FPS Counter
    Planning to add the second variant of calculation via time and to update reset button function

--- Project_1/Assets/Scripts/FPS/FPSCounter.cs
+++ Project_1/Assets/Scripts/FPS/FPSCounter.cs
@@ -1,15 +1,20 @@
+using System.Linq;
 using UnityEngine;
 
 public class FPSCounter : MonoBehaviour
 {
     private int _bufferSize;
     private int[] _fpsBuffer;
+    private int[] _sortedFpsBuffer;
     private int _fpsBufferIndex;
-
+    
+    private const float FifthPercentile = 5f;
+    private const float FirstPercentile = 1f;
+    
     public int CurrentFPS { get; private set; }
     public int AverageFPS { get; private set; }
-    public int Worst5PercentFPS { get; }
-    public int Worst1PercentFPS { get; }
+    public int Worst5PercentFPS { get; private set; }
+    public int Worst1PercentFPS { get; private set; }
 
     private void Start()
     {
@@ -39,13 +44,19 @@ public class FPSCounter : MonoBehaviour
     private void CalculateFPS()
     {
         var sum = 0;
-
         for (var i = 0; i < _bufferSize; i++) sum += _fpsBuffer[i];
 
         AverageFPS = sum / _bufferSize;
+        Worst5PercentFPS = CalculatePercentiles(FifthPercentile);
+        Worst1PercentFPS = CalculatePercentiles(FirstPercentile);
     }
 
-    private void CalculatePercentiles()
+    private int CalculatePercentiles(float percentile)
     {
+        _sortedFpsBuffer = _fpsBuffer.OrderBy(x=>x).ToArray();
+        
+        int sequenceNumPercentile = Mathf.RoundToInt((percentile * _bufferSize) / 100);
+   
+        return _sortedFpsBuffer[sequenceNumPercentile];
     }
 }
