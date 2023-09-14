0278af8cf33ddff0a6b6dd1d581d52706c1c2ef4
Aigerim Almazova <71431806+moonbell-e@users.noreply.github.com>
2023-09-14 15:18:22 +0300

    Fixed the interval for FPS showing

--- Project_1/Assets/Scripts/FPS/FPSShower.cs
+++ Project_1/Assets/Scripts/FPS/FPSShower.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class FPSShower : MonoBehaviour
 {
+    [SerializeField] private float _fpsTextUpdateInterval;
     [SerializeField] private TextMeshProUGUI _currentFPS;
     [SerializeField] private TextMeshProUGUI _averageFPS;
     [SerializeField] private TextMeshProUGUI _worst5FPS;
@@ -13,6 +14,7 @@ public class FPSShower : MonoBehaviour
     [SerializeField] private Button _startFPSButton;
     [SerializeField] private FPSCounter _fpsCounter;
 
+    private float _lastFPSTextUpdateTime;
     private bool _isShowFPS;
 
     private void Start()
@@ -23,14 +25,20 @@ public class FPSShower : MonoBehaviour
 
     private void Update()
     {
-        if (!_isShowFPS) return;
-
-        _currentFPS.text = _fpsCounter.CurrentFPS.ToString();
-        _averageFPS.text = _fpsCounter.AverageFPS.ToString();
-        _worst5FPS.text = _fpsCounter.Worst5PercentFPS.ToString();
-        _worst1FPS.text = _fpsCounter.Worst1PercentFPS.ToString();
+        if (!_isShowFPS || !(Time.unscaledTime - _lastFPSTextUpdateTime >= _fpsTextUpdateInterval)) return;
+        
+        UpdateText();
+        _lastFPSTextUpdateTime = Time.unscaledTime;
     }
 
+    private void UpdateText()
+    {
+        _currentFPS.text = _fpsCounter.CurrentFPS.ToString();
+        _averageFPS.text = _fpsCounter.AverageFPS.ToString();
+        _worst5FPS.text = _fpsCounter.Worst5PercentFPS >= 1 ? _fpsCounter.Worst5PercentFPS.ToString() : "N/A";
+        _worst1FPS.text = _fpsCounter.Worst1PercentFPS >= 1 ? _fpsCounter.Worst1PercentFPS.ToString() : "N/A";
+    }
+    
     private void ShowFPS()
     {
         _isShowFPS = true;
@@ -41,7 +49,7 @@ public class FPSShower : MonoBehaviour
     {
         _isShowFPS = false;
         _fpsCounter.StopFPSCalculation();
-        
+
         _currentFPS.text = "0";
         _averageFPS.text = "0";
         _worst5FPS.text = "0";
